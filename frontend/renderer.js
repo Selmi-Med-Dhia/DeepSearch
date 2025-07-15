3da9dc63639b829da8d97691b1cbbb01c3bf5379
Selmi Med Dhia <monjihidousi@gmail.com>
2025-07-15 23:06:03 +0100

    first sidebar working

--- frontend/renderer.js
+++ frontend/renderer.js
@@ -1,4 +1,4 @@
-const { ipcRenderer } = require('electron');
+//const { ipcRenderer } = require('electron');
 try {
     const $ = require('jquery');
     window.$ = window.jQuery = $;
@@ -15,3 +15,23 @@ async function selectFolder() {
     console.log('Selected folder:', folderPath);
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const gearBtn = document.getElementById('gear-button');
+  const homeBtn = document.getElementById('settings-home-button');
+  const sidebar = document.getElementById('sidebar');
+
+  let isAnimating = false;
+  openclose = () => {
+    if (isAnimating) return;
+
+    isAnimating = true;
+    sidebar.classList.toggle('active');
+
+    setTimeout(() => {
+      isAnimating = false;
+    }, 500);
+  };
+  gearBtn.addEventListener('click', openclose)
+  homeBtn.addEventListener('click', openclose)
+});
